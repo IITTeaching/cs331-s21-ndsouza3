b2742a61fc2dda4c3a820da0880aa3873df46d02
Creativename72 <neilgregdsouza@gmail.com>
2021-01-22 15:23:42 -0600

    Update lab01.py

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -23,7 +23,7 @@ def captured_output():
 # implement this function
 def is_perfect(n):
     total = 0
-    for i in range(n + 1//2):
+    for i in range(1,n + 1//2):
         if n % i == 0:
             total += i
     return n == total
